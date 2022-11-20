046e84c0286ead1ed5e13049dc348b3a54cf7a08
Rodolfo Londero <rodolfopl@gmail.com>
2022-11-20 01:23:23 -0300

    Arquitetura MVVM (Model-View-VielModel)

---
+++ 18-MVVM/app/src/main/java/com/example/mvvm/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.mvvm
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    private var textWelcome = MutableLiveData<String>()
+    private var login = MutableLiveData<Boolean>()
+    private val personRepository = PersonRepository()
+
+    init {
+        textWelcome.value = "Olá!"
+    }
+
+    fun welcome(): LiveData<String>{
+        return textWelcome
+    }
+
+    fun login(): LiveData<Boolean>{
+        return login
+    }
+
+    fun doLogin(email: String, password: String) {
+        login.value = personRepository.login(email, password)
+    }
+
+}
---
+++ 18-MVVM/app/src/main/java/com/example/mvvm/PersonRepository.kt
@@ -0,0 +1,8 @@
+package com.example.mvvm
+
+class PersonRepository {
+
+    fun login(email: String, password: String): Boolean{
+        return (email != "" && password != "")
+    }
+}
