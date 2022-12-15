51d97a37b7653db88e223a3c5ad22e4c38c7223a
Rodolfo Londero <rodolfopl@gmail.com>
2022-12-14 23:22:58 -0300

    Desenvolvimento do login

--- 21-Tasks/app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
+++ 21-Tasks/app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.devmasterteam.tasks.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.ActivityLoginBinding
 import com.devmasterteam.tasks.viewmodel.LoginViewModel
 
@@ -26,13 +29,43 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonLogin.setOnClickListener(this)
         binding.textRegister.setOnClickListener(this)
 
+        viewModel.verifyLoggedUser()
+
         // Observadores
         observe()
     }
 
     override fun onClick(v: View) {
+        if (v.id == R.id.button_login) {
+            handleLogin()
+        }
     }
 
     private fun observe() {
+        viewModel.login.observe(this) {
+            if (it.status()) {
+                goToMain()
+            } else {
+                Toast.makeText(applicationContext, it.message(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.loggedUser.observe(this) {
+            if (it) {
+                goToMain()
+            }
+        }
+    }
+
+    private fun goToMain() {
+        startActivity(Intent(applicationContext, MainActivity::class.java))
+        finish()
+    }
+
+    private fun handleLogin() {
+        val email = binding.editEmail.text.toString()
+        val password = binding.editPassword.text.toString()
+
+        viewModel.doLogin(email, password)
     }
 }
